bcf91adb2f3764eb3a9f932d120c9f26c2964b39
canonhor93 <46227075+canonhor93@users.noreply.github.com>
2020-06-13 12:13:57 +0800

    Add files via upload

---
+++ custom_gtl/__manifest__.py
@@ -0,0 +1,32 @@
+# See LICENSE file for full copyright and licensing details.
+
+{
+    # Module Info.
+    "name": "",
+    "version": "12.0.1.0.0",
+    "category": "",
+    "license": 'OPL-1',
+    "summary": """""",
+    "description": """""",
+
+    # Author
+    "author": "Excelroot Technology Sdn Bhd",
+    "website": "https://www.excelroot.com/",
+
+    # Dependencies
+    "depends": ['product', 'account'],
+
+    # Data
+    "data": [
+        'security/freight_security.xml',
+    ],
+
+    # Odoo App Store Specific
+    'images': ['static/description/odoo-app-freight.jpg'],
+
+    # Technical
+    "application": True,
+    "installable": True,
+    'price': ,
+    'currency': 'EUR',
+}
---
+++ custom_gtl/models/__init__.py
@@ -0,0 +1,5 @@
+# See LICENSE file for full copyright and licensing details.
+"""Initialize the python files."""
+
+from . import freight_config
+
